829227beaca6276a4f88b4bbe4e9db84d2a6c78c
EliasDev <elias.dottori@hotmail.com>
2022-09-07 03:13:34 -0300

    Creacion Home

---
+++ components/Home/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import estilos from "./HomePage.module.css";
+
+const HomePage = () => {
+  return (
+    <>
+        <div className="">
+            <div className=""></div>
+
+              <a href="#">Gastronómico</a>
+        </div>
+        </>
+  );
+};
+
+export default HomePage;
