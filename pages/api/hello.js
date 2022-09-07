592faf6f19234bacb5655c5e8ead545435730b4d
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-09-07 04:05:40 -0300

    db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    return await createCategorias(req, res);
+  } else {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed", success: false });
+  }
+}
+
+async function createCategorias(req, res) {
+  const body = req.body;
+  try {
+    const newEntry = await prisma.categorias.create({
+      data: {
+        nombre: body.nombre,
+        sistema: body.sistema,
+        estado: body.estado,
+      },
+    });
+    return res.status(200).json(newEntry, { success: true });
+  } catch (error) {
+    console.error("Request error", error);
+    res.status(500).json({ error: "Error creating question", success: false });
+  }
 }
