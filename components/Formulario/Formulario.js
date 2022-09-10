2fc564a9ed10d27e46b794f70bc974c440ab8e99
lucflores <luciaflores0609@gmail.com>
2022-09-10 20:50:50 -0300

    arreglos de formulario

--- components/Formulario/Formulario.js
+++ components/Formulario/Formulario.js
@@ -18,35 +18,35 @@ const Formulario = () => {
   return (
     <div className="w-full bg-gray-800 h-screen">
     <div className="bg-gradient-to-b from-black-800 to-black-600 h-96"></div>
-    <div className="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
-        <div className="bg-gray-900 w-full shadow rounded p-8 sm:p-12 -mt-72">
-            <p className="text-3xl font-bold leading-7 text-center text-white">Sugerencias!!</p>
+    <div className="mx-auto">
+        <div className="bg-gray-900 w-full shadow rounded p-8 sm:p-12 -mt-72 h-screen">
+            <p className="text-3xl font-bold leading-7 text-center text-white">SUGERENCIAS</p>
             <form onSubmit={sendEmail} action="" method="post">
                 <div className="md:flex items-center mt-12">
                     <div className="w-full md:w-1/2 flex flex-col">
                         <label className="font-semibold leading-none text-gray-300">Nombre del local</label>
-                        <input type="text" name="user_name" className="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
+                        <input type="text" name="user_name" className="border border-green-500 leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" required />
                     </div>
                     <div className="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
-                        <label className="font-semibold leading-none text-gray-300">Telefono</label>
-                        <input type="tel" name="user_number" className="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"/>
+                        <label className="font-semibold leading-none text-gray-300">Teléfono</label>
+                        <input type="tel" name="user_number" className="border border-green-500 leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" required />
                     </div>
                 </div>
                 <div className="md:flex items-center mt-8">
                     <div className="w-full flex flex-col">
                         <label className="font-semibold leading-none text-gray-300">Email</label>
-                        <input type="email" name="user_email" className="leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"/>
+                        <input type="email" name="user_email" className=" border border-green-500 leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" required />
                     </div>
                     
                 </div>
                 <div>
                     <div className="w-full flex flex-col mt-8">
                         <label className="font-semibold leading-none text-gray-300">Mensaje</label>
-                        <textarea type="text" name="user_message" className="h-40 text-base leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-800 border-0 rounded"></textarea>
+                        <textarea type="text" name="user_message" className="border border-green-500 h-40 text-base leading-none text-gray-50 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-800 border-0 rounded"></textarea>
                     </div>
                 </div>
                 <div className="flex items-center justify-center w-full">
-                    <button className="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                    <button className="mt-20 font-semibold leading-none text-white py-5 px-20 bg-gradient-to-r from-green-500 to-blue-500 rounded rounded-full">
                         ENVIAR
                     </button>
                 </div>
