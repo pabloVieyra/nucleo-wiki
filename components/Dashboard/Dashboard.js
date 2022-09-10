fb10c5975f115a7d0175fd23a5774233bca91efe
gusfloresok <90418192+gusfloresok@users.noreply.github.com>
2022-09-10 18:41:58 -0300

    Dashborad inicios

--- components/Dashboard/Dashboard.js
+++ components/Dashboard/Dashboard.js
@@ -1,7 +1,64 @@
 import React from "react";
+import estilos from "./Dashboard.module.css";
+import Image from 'next/image';
+import logocol from '../../public/Images/col.png';
+/* import logo1 from './public/Images/col.png';
+import logong from "./public/Images/";
+import logog1 from './public/Images/';
+import logochk from '../../public/Images/logo.png';
+import logofce from '../../public/Images/logo.png'; */
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+return (
+    
+        <nav className={estilos.contenedor}>
+            <div className={estilos.contenedor2}>
+                <ul>
+                    <li className={estilos.logoNucleo}>
+                        <div className={estilos.logo1}></div>
+                    </li>
+
+                    <li className={estilos.dropLista}><a href="#">HOLA</a>
+                        <ul>
+                            <li><a href="#">Crear</a></li>
+                            <li><a href="#">Modificar</a></li>
+                            <li><a href="#">Borrrar</a></li>
+                            <li><a href="#">Ejemplo</a></li>
+                        </ul>
+                    </li>
+
+                    <li className={estilos.dropLista}><a href="#">HOLA</a>
+                        <ul>
+                            <li><a href="#">Crear</a></li>
+                            <li><a href="#">Modificar</a></li>
+                            <li><a href="#">Borrrar</a></li>
+                            <li><a href="#">Ejemplo</a></li>
+                        </ul>
+                    </li>
+
+                    <li className={estilos.dropLista}><a href="#">HOLA</a>
+                        <ul>
+                            <li><a href="#">Crear</a></li>
+                            <li><a href="#">Modificar</a></li>
+                            <li><a href="#">Borrrar</a></li>
+                            <li><a href="#">Ejemplo</a></li>
+                        </ul>
+                    </li>
+
+                    <li className={estilos.dropLista}><a href="#">HOLA</a>
+                        <ul>
+                            <li><a href="#">Crear</a></li>
+                            <li><a href="#">Modificar</a></li>
+                            <li><a href="#">Borrrar</a></li>
+                            <li><a href="#">Ejemplo</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    
+);
 };
 
 export default Dashboard;
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,13 @@ import Image from "next/image";
 import Formulario from "../components/Formulario/Formulario";
 import NavBar from "../components/NavBar/NavBar";
 import HomePage from "../components/Home/HomePage";
+import Dashboard from "../components/Dashboard/Dashboard";
 
 export default function Home() {
   return (
     <>
-      <NavBar />
-      <HomePage></HomePage>
+      {/* <NavBar /> */}
+      <Dashboard />
     </>
   );
 }
