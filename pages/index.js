1021dcec709d43b0e2eac118f2780103a062a63a
lucflores <luciaflores0609@gmail.com>
2022-09-19 18:48:52 -0300

    footer vs I

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import Formulario from "../components/Formulario/Formulario";
 import NavBar from "../components/NavBar/NavBar";
 import HomePage from "../components/Home/HomePage";
 import Dashboard from "../components/Dashboard/Dashboard";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
-      {/* <NavBar /> */}
-      <Dashboard />
+      <NavBar />
+      <HomePage />
+      <Footer />
     </>
   );
 }
