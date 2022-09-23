3aeebdc08db5c340ecc2bad2f90ad906a58d377a
Elias Dottori <elias.dottori@hotmail.com>
2022-09-23 19:03:10 -0300

    Components post 1v

---
+++ components/Post/Sub1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sub1 = () => {
+  return <div>Sub1</div>;
+};
+
+export default Sub1;
---
+++ components/Post/Sub2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sub2 = () => {
+  return <div>Sub2</div>;
+};
+
+export default Sub2;
