a09e0e62131b53b91d19ddc19e251b0720fa1e94
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-09-22 21:14:37 -0300

    Page DashboardP

---
+++ pages/DashboardP.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "../components/Dashboard/Dashboard";
+
+const DashboardP = () => {
+  return (
+    <div>
+      <Dashboard></Dashboard>
+    </div>
+  );
+};
+
+export default DashboardP;
