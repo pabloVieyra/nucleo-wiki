ddff0a62f729ca54d42d90d59be6cfc498b4d7b0
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-09-29 12:22:29 -0300

    changes en index

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import NavBar from "../components/NavBar/NavBar";
 import HomePage from "../components/Home/HomePage";
 import Dashboard from "../components/Dashboard/Dashboard";
 import AppLayout from "../components/AppLayout/AppLayout";
+import Head from "next/head";
 
 export default function Home() {
   return (
