08d3405524a7fdfb828452c5a658fd6383b73b97
Elias Dottori <elias.dottori@hotmail.com>
2022-09-29 02:25:40 -0300

    Pruebas

--- pages/Post.js
+++ pages/Post.js
@@ -5,12 +5,10 @@ import H1Post from "../components/Post/H1";
 import H2Post from "../components/Post/H2";
 import H3Post from "../components/Post/H3";
 import Ruta from "../components/Post/Ruta";
-import NavBar from "../components/NavBar/NavBar";
 
 const Post = () => {
   return (
     <div>
-      <NavBar />
       <Ruta></Ruta>
       <H1Post></H1Post>
       <H2Post></H2Post>
