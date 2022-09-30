4f2841f119ae196f9c701941534e06d7f23f7a69
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-09-30 02:57:47 -0300

    Componentizando

--- components/Homedash/Homedash.js
+++ components/Homedash/Homedash.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Image from "next/image";
-import simbolo from "../../public/Images/simbolo.png";
+
 import CardHome from "../CardHome/CardHome";
+import CardNewPost from "../CardHome/CardNewPost";
 
 const Homedash = () => {
   return (
@@ -12,14 +13,7 @@ const Homedash = () => {
           <CardHome></CardHome>
           <CardHome></CardHome>
           <CardHome></CardHome>
-          <div className=" contentsflex inline-block flex-col border rounded-lg overflow-hidden bg-white my-12 mx-6 ">
-            <div className="p-12 text-gray-600">
-              {/* Texto */}
-              <div className="flex flex-row text-xl items-center justify-center">
-                <Image src={simbolo} alt="SimboloMas"></Image>
-              </div>
-            </div>
-          </div>
+          <CardNewPost></CardNewPost>
         </div>
       </div>
     </>
