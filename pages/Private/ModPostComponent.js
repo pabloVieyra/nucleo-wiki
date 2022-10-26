b9646739d01e979ea190f4ea387822cf6e752820
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2022-10-26 12:34:10 -0300

    changes post combination

--- pages/Private/ModPostComponent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ModPostComponent = () => {
-  return <div>ModPostComponent</div>;
-};
-
-export default ModPostComponent;
---
+++ pages/api/post/porSistema/[sid].js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    return await traerPostPorCategoria(req, res);
+  } else {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed", success: false });
+  }
+}
+
+async function traerPostPorCategoria(req, res) {
+  try {
+    const { sid } = req.query;
+    const Posts = await prisma.post.findMany({
+      where: { sistema: Number(sid) },
+    });
+    return res.status(200).json(Posts, { success: true });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ error: "Error reading from database", success: false });
+  }
+}
